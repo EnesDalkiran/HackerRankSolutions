a1e6e2b40acad0a1c61360cecc8cbadb0c110e39
enesd <enesd@DESKTOP-SJFRGMJ>
2022-11-17 20:19:24 +0300

    Add project files.

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SimpleArraySum;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(simpleArraySum(new List<int>() { 1, 2, 3 }));
+    }
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        for (int i = 0; i < ar.Count; i++)
+        {
+
+            sum += ar[i];
+
+        }
+        return sum;
+
+    }
+
+}
