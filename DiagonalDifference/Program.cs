2e6d4d0add06dab2d2f2aaeb062484357d5121ae
EnesDalkiran <enes.dalkiran75@gmail.com>
2022-11-17 21:43:29 +0300

    diagonal difference solution

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DiagonalDifference;
+class Program
+{
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sum = 0;
+        int sum2 = 0; ;
+        for (int i = 0; i < arr.Count; i++)
+        {
+            sum += arr[i][i];
+            sum2 += arr[i][(arr.Count - 1) - i];
+
+        }
+        return Math.Abs(sum - sum2);
+    }
+}
